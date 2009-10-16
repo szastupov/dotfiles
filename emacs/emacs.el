890ef3249f9f8e3351d81b2bceec01690a920a60
Stepan Zastupov <redchrom@gmail.com>
2009-10-17 03:09:25 +0400

    New colorscheme

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -13,10 +13,8 @@
  '(compilation-window-height 10)
  '(delete-selection-mode t)
  '(fringe-mode (quote (nil . 0)) nil (fringe))
- '(gud-tooltip-mode t)
  '(indicate-empty-lines t)
  '(inhibit-startup-screen t)
- '(menu-bar-mode nil)
  '(mouse-wheel-mode t)
  '(quack-default-program "ypsilon")
  '(quack-fontify-style (quote plt))
@@ -37,9 +35,31 @@
  '(tool-bar-mode nil)
  '(tooltip-delay 0.5)
  '(tooltip-mode t)
- '(tuareg-library-path "/usr/lib64/ocaml")
- '(xterm-mouse-mode t)
- '(xterm-title-icon-title-format nil))
+ '(tuareg-library-path "/usr/lib64/ocaml"))
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#0C1021" :foreground "#F8F8F8"))))
+ '(font-lock-builtin-face ((((class color) (min-colors 88) (background dark)) (:foreground "#F8F8F8"))))
+ '(font-lock-comment-delimiter-face ((default (:inherit font-lock-comment-face)) (((class color) (min-colors 8) (background light)) nil)))
+ '(font-lock-comment-face ((nil (:foreground "#AEAEAE"))))
+ '(font-lock-constant-face ((((class color) (min-colors 88) (background dark)) (:foreground "#D8FA3C"))))
+ '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background dark)) (:foreground "#FF6400"))))
+ '(font-lock-keyword-face ((((class color) (min-colors 8)) (:foreground "#FBDE2D"))))
+ '(font-lock-preprocessor-face ((t (:foreground "Aquamarine"))))
+ '(font-lock-regexp-grouping-backslash ((t (:foreground "#E9C062"))))
+ '(font-lock-regexp-grouping-construct ((t (:foreground "red"))))
+ '(font-lock-string-face ((((class color) (min-colors 8)) (:foreground "#61CE3C"))))
+ '(font-lock-type-face ((((class color) (min-colors 88) (background dark)) (:foreground "#8DA6CE"))))
+ '(font-lock-variable-name-face ((((class color) (min-colors 8)) (:foreground "#FF6400"))))
+ '(highlight ((((class color) (min-colors 88) (background dark)) (:background "#222222"))))
+ '(mode-line ((((class color) (min-colors 88)) (:background "grey75" :foreground "black"))))
+ '(region ((((class color) (min-colors 88) (background dark)) (:background "#253B76")))))
+
 
 (defvar tmp-autosave-dir "~/.emacs.d/autosave/")
 
@@ -49,33 +69,15 @@
 (defvar backup-dir "~/.emacs.d/backup/")
 (setq backup-directory-alist (list (cons "." backup-dir)))
 
-(require 'info)
-
 (require 'iswitchb)
 (dolist (b '("*Messages*" "*Completions*" "*Buffer List*"))
   (add-to-list 'iswitchb-buffer-ignore b))
 (iswitchb-mode 1)
 
-(load "~/dotfiles/emacs/devel")
-
 (when (fboundp 'windmove-default-keybindings)
       (windmove-default-keybindings))
 
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(font-lock-comment-delimiter-face ((default (:inherit font-lock-comment-face)) (((class color) (min-colors 8) (background light)) nil)))
- '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "blue"))))
- '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
- '(font-lock-keyword-face ((((class color) (min-colors 8)) (:foreground "yellow"))))
- '(font-lock-string-face ((((class color) (min-colors 8)) (:foreground "red"))))
- '(font-lock-variable-name-face ((((class color) (min-colors 8)) nil))))
-
-(setq x-select-enable-clipboard t)
 (setq frame-title-format '("" "%b%& - emacs"))
-(require 'xterm-title)
 
-(unless window-system
-  (xterm-title-mode 1))
+(load "~/dotfiles/emacs/devel")
+

98267cc8cf4695a250404727902a01722932b134
Stepan Zastupov <redchrom@gmail.com>
2010-01-23 01:59:48 +0300

    [emacs] Added cedet

--- emacs/devel.el
+++ emacs/devel.el
@@ -58,3 +58,19 @@
 ;;Haskell
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;; Cedet
+(load "/home/redchrom/src/cedet/common/cedet.el")
+(semantic-load-enable-code-helpers)
+(require 'semantic-ia)
+(require 'semantic-gcc)
+
+(defun my-cedet-hook ()
+  (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  (local-set-key "\C-cq" 'semantic-ia-show-doc)
+  (local-set-key "\C-cs" 'semantic-ia-show-summary))
+
+(add-hook 'c-mode-common-hook 'my-cedet-hook)

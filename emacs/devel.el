87f3d6171a638a0a7c263fe58f53a8470776b30c
Stepan Zastupov <redchrom@gmail.com>
2010-06-06 00:34:06 +0400

    Use cedet shipped with emacs 23.2

--- emacs/devel.el
+++ emacs/devel.el
@@ -1,7 +1,7 @@
 (defun make-cur ()
   (interactive)
   (compile
-   (concat "make "
+   (concat "make CFLAGS='-Wall -ggdb' "
 		   (car (split-string (buffer-name) "\\.")))))
 
 (defun devel-hook ()
@@ -62,7 +62,6 @@
 ;; Cedet
 
 (defun my-cedet-hook ()
-  (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
   (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
   (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
   (local-set-key "\C-cj" 'semantic-ia-fast-jump)
@@ -76,25 +75,9 @@
 		 (output (shell-command-to-string cmd)))
 	(split-string (replace-regexp-in-string "-I" "" output))))
 
-(defun c-libraries-load ()
-  (interactive)
-  (load "~/src/cedet/common/cedet.el")
-  (semantic-load-enable-code-helpers)
-  (require 'semantic-ia)
-  (require 'semantic-gcc)
-
-  (setq-mode-local c-mode semanticdb-find-default-throttle
-				   '(project unloaded system recursive))
-  (setq-mode-local c++-mode semanticdb-find-default-throttle
-				   '(project unloaded system recursive))
-
-  (require 'semanticdb-ectag)
-  (semantic-load-enable-primary-exuberent-ctags-support)
-
-  (global-ede-mode t)
-  (let ((pf "~/Projects/ede.el"))
-	(if (file-exists-p pf)
-		(load pf)))
-  )
-
-(add-hook 'c-initialization-hook 'c-libraries-load)
+(global-ede-mode 1)
+(require 'semantic/sb)
+(semantic-mode 1)
+(let ((pf "~/Projects/ede.el"))
+  (if (file-exists-p pf)
+	  (load pf)))
